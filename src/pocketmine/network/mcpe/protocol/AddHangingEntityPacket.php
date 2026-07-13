1f023504a596c0778e1285da2128a382e6f728e6
XinYueNeko <3649745663@qq.com>
2026-07-13 09:36:20 +0800

    完善一些对接新版APi的

---
+++ src/pocketmine/network/mcpe/protocol/AddHangingEntityPacket.php
@@ -0,0 +1,6 @@
+<?php
+namespace pocketmine\network\mcpe\protocol;
+
+use pocketmine\network\protocol\AddHangingEntityPacket as Original;
+
+class AddHangingEntityPacket extends Original {}
---
+++ src/pocketmine/network/mcpe/protocol/AddItemEntityPacket.php
@@ -0,0 +1,6 @@
+<?php 
+namespace pocketmine\network\mcpe\protocol;
+
+use pocketmine\network\protocol\AddItemEntityPacket as Original;
+
+class AddItemEntityPacket extends Original {}
